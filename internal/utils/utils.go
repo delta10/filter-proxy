3f383ad3e2d8f631fe18a0da29a4978b163a33a7
Bart Jeukendrup <bart@jeukendrup.nl>
2024-02-20 21:40:15 +0100

    fix: do not use X-Real-IP

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -84,10 +84,7 @@ func EnvSubst(input string) string {
 }
 
 func ReadUserIP(r *http.Request) string {
-	IPAddress := r.Header.Get("X-Real-Ip")
-	if IPAddress == "" {
-		IPAddress = r.Header.Get("X-Forwarded-For")
-	}
+	IPAddress := r.Header.Get("X-Forwarded-For")
 	if IPAddress == "" {
 		IPAddress = r.RemoteAddr
 	}
