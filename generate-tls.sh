09e05f3e9e51f5b3ada5b10543cc7cb1268b3833
Bart Jeukendrup <bart@jeukendrup.nl>
2024-03-08 16:18:53 +0100

    Add scripts for generating TLS

---
+++ generate-tls.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cfssl gencert -initca ca-csr.json | cfssljson -bare ca
+
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -profile=server \
+  tls-csr.json | cfssljson -bare tls
